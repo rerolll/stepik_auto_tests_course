b66744d7ea80bfa509ba074c5356ec210ca51b1e
It's me <kosmakovie@gmail.com>
2024-02-12 17:38:29 +0300

    update requirements, update 3_2_unittest

--- 3_2_unittest.py
+++ 3_2_unittest.py
@@ -10,55 +10,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+def link_t(link):
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element(
+        By.CSS_SELECTOR, ".first_block .first").send_keys("Ivan")
+
+    browser.find_element(
+        By.CSS_SELECTOR, ".first_block .second").send_keys("Petrov")
+
+    browser.find_element(
+        By.CSS_SELECTOR, ".first_block .third").send_keys("Smolensk")
+
+    browser.find_element(
+        By.CSS_SELECTOR, "button.btn").click()
+
+    return browser.find_element(By.TAG_NAME, "h1").text
+
+
 class TestAbs(unittest.TestCase):
+
     def test_1(self):
-        link = "http://suninjuly.github.io/registration1.html"
-        browser = webdriver.Chrome()
-        browser.get(link)
-
-        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
-        input1.send_keys("Ivan")
-
-        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
-        input2.send_keys("Petrov")
-
-        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
-        input3.send_keys("Smolensk")
-
-        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-        button.click()
-
-        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-        welcome_text = welcome_text_elt.text
-
-        browser.quit()
         self.assertEqual(
-            welcome_text, "Congratulations! You have successfully registered!"
+            link_t("http://suninjuly.github.io/registration1.html"),
+            "Congratulations! You have successfully registered!",
+            "registration is failed",
         )
 
     def test_2(self):
-        link = "http://suninjuly.github.io/registration2.html"
-        browser = webdriver.Chrome()
-        browser.get(link)
-
-        input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
-        input1.send_keys("Ivan")
-
-        input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
-        input2.send_keys("Petrov")
-
-        input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
-        input3.send_keys("Smolensk")
-
-        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-        button.click()
-
-        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-        welcome_text = welcome_text_elt.text
-
-        browser.quit()
         self.assertEqual(
-            welcome_text, "Congratulations! You have successfully registered!"
+            link_t("http://suninjuly.github.io/registration2.html"),
+            "Congratulations! You have successfully registered!",
+            "registration is failed",
         )
 
 
