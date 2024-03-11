dbf25be7fba2c83e62228534413b95f071083ccd
Ilya Kosmakov <kosmakovie@gmail.com>
2024-03-11 10:54:32 +0300

    update conftest.py, add test_parser

--- section3/3_6_5_firefox.py
+++ section3/3_6_5_firefox.py
@@ -3,7 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
-#driver = webdriver.Firefox(executable_path = 'C:/geckodriver')
 driver = webdriver.Firefox(
     service=FirefoxService(GeckoDriverManager().install()))
 driver.get("https://stepik.org/lesson/25969/step/8")
+
+"""https://github.com/SergeyPirogov/webdriver_manager
+driver = webdriver.Firefox(executable_path = 'C:/geckodriver')"""
