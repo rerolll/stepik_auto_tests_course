8e67bfcee1790fbf4645cb91218a4628dada3f9b
Ilya Kosmakov <kosmakovie@gmail.com>
2024-03-11 12:18:56 +0300

    update conftest add option language

--- section3/conftest.py
+++ section3/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
@@ -10,9 +12,15 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--browser_name",
+        "--language",
         action="store",
         default=None,
+        help="Choose language: '--language=en-gb' or '--language=ru'",
+    )
+    parser.addoption(
+        "--browser_name",
+        action="store",
+        default="chrome",
         help="Choose browser: chrome or firefox",
     )
 
@@ -21,20 +29,33 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
+
+    user_language = request.config.getoption("language")
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
+
+        options = ChromeOptions()
+        options.add_experimental_option(
+            "prefs", {"intl.accept_languages": user_language}
+        )
+
         service = ChromeService(
             executable_path=ChromeDriverManager().install()
         )
-        browser = webdriver.Chrome(service=service)
+        browser = webdriver.Chrome(service=service, options=options)
 
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
 
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", user_language)
+
         browser = webdriver.Firefox(
             service=FirefoxService(
                 executable_path=GeckoDriverManager().install()
-            )
+            ),
+            options=options,
         )
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
